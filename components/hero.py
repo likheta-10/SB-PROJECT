6668caa4440e54edb007256f93fe60e94b103fcf
Vadlamudi Dinesh <dineshvadlamudi05@gmail.com>
2026-07-13 20:07:50 +0530

    Delete components directory

--- components/hero.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def hero():
-    st.markdown(
-        "<h1 style='text-align: center; font-size: 28px;'>Voice-Based Concept Understanding Analyzer</h1>", 
-    unsafe_allow_html=True
-)
-
-    
